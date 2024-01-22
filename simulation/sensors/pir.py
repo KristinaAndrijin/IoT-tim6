ea4022cbf0dcb203c8a73db2f6f7a09b6132520a
VladimirGolosin <96448683+VladimirGolosin@users.noreply.github.com>
2024-01-22 16:15:56 +0100

    male promene sa konsultacija

--- simulation/sensors/pir.py
+++ simulation/sensors/pir.py
@@ -6,9 +6,10 @@ class PassiveInfraRed:
         self.pir_pin = pin
         self.callback = callback
         self.settings = settings
+        GPIO.cleanup()
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pir_pin, GPIO.IN)
-    def motion_detected(self):
+    def motion_detected(self, dummy):
         self.callback(True, self.settings)
         # print("You moved")
     def no_motion(self):
@@ -16,6 +17,12 @@ class PassiveInfraRed:
         # print("You stopped moving")
 
     def run(self, delay):
+        GPIO.cleanup()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pir_pin, GPIO.IN)
+
+        # GPIO.add_event_detect(self.pir_pin, GPIO.BOTH, callback=self.motion_detected)
+        # poslaće arg da li je rising ili falling pa handleuj
         GPIO.add_event_detect(self.pir_pin, GPIO.RISING, callback=self.motion_detected)
         # GPIO.add_event_detect(self.pir_pin, GPIO.FALLING, callback=self.no_motion)
         # input("Press any key to exit...")
