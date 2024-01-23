dbb4b5e515af022ecf13c289b82d28facaa7d709
VladimirGolosin <96448683+VladimirGolosin@users.noreply.github.com>
2024-01-23 20:24:38 +0100

    osnova za frontend

---
+++ frontend/iot-tim6/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DevicesComponent } from './devices/devices.component';
+import { AlarmsComponent } from './alarms/alarms.component';
+import { TimersComponent } from './timers/timers.component';
+import { ManageComponent } from './manage/manage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    DevicesComponent,
+    AlarmsComponent,
+    TimersComponent,
+    ManageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
